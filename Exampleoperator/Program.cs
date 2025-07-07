b91dbb89eab256feb2a0f411ac07fd1d4db8447a
TA <162087971+tagamemobile@users.noreply.github.com>
2025-07-07 21:10:21 +0700

    add
    add

---
+++ Exampleoperator/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("Hay nhap gia tri chieu rong");
+        width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Hay nhap gia tri chieu cao");
+        height = Convert.ToInt32(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine($"Gia tri dien tich la {area}");
+    
+    }
+}
